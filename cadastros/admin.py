8d56e36144d3f74e828be8c08170f17051ec545c
Max Muller <myllermax61@gmail.com>
2025-05-20 20:45:03 -0300

    criação do app cadastros

---
+++ cadastros/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Cliente, Sistema, Contrato
+
+admin.site.register(Cliente)
+admin.site.register(Sistema)
+admin.site.register(Contrato)
+
+# Register your models here.
---
+++ cadastros/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Sistema(models.Model):
+    nome = models.CharField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Contrato(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name="contrato")
+    sistema = models.ForeignKey(Sistema, on_delete=models.CASCADE, related_name="contrato")
+    valor = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return f"{self.cliente} - {self.sistema}"
