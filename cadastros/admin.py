b8f86ed6913bd08bb542f66138a292a4ac0c4069
Max Muller <myllermax61@gmail.com>
2025-05-27 21:41:33 -0300

    configuração do admin

--- cadastros/admin.py
+++ cadastros/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from .models import Cliente, Sistema, Contrato
 
 admin.site.register(Cliente)
-admin.site.register(Sistema)
-admin.site.register(Contrato)
+
+
+
+@admin.register(Contrato)
+class SiteContrato(admin.ModelAdmin):
+    list_display = ["cliente", "sistema", "valor", "inicio", "termino"]
+
+
+@admin.register(Sistema)
+class SiteSistema(admin.ModelAdmin):
+    list_display = ["nome"]
 
 # Register your models here.
