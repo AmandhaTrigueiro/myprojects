7fae528edd155c1d3f3dbd1891a5c93a1dc291c2
Amanda Pereira <amanda.pereira.trigueiro@gmail.com>
2025-05-27 21:41:33 -0300

    configuração do admin

--- cadastros/models.py
+++ cadastros/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Cliente(models.Model):
     nome = models.CharField()
+    cnpj = models.CharField(null=True, blank=True)
 
     def __str__(self):
         return self.nome
@@ -19,6 +20,8 @@ class Contrato(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name="contrato")
     sistema = models.ForeignKey(Sistema, on_delete=models.CASCADE, related_name="contrato")
     valor = models.DecimalField(decimal_places=2, max_digits=10)
+    inicio = models.DateField()
+    termino = models.DateField()
 
     def __str__(self):
         return f"{self.cliente} - {self.sistema}"
